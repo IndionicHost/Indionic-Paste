c41c95bc690b2e80bce0fcc0c9e55d4bef117480
Anas Shaikh <itsanas1554@gmail.com>
2022-07-06 16:30:35 +0530

    Script

---
+++ install.sh
@@ -0,0 +1,29 @@
+apt update -y && apt upgrade -y
+
+clean
+
+mkdir -p /var/www/
+
+git clone https://github.com/IndionicHost/Indionic-Paste.git
+
+clean
+
+cd Indionic-Paste
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+apt install -y nodejs
+
+clean 
+
+cp config.demo.json config.json
+
+npm install
+
+mkdir hastes
+
+npm install pm2 -g
+
+clean
+
+pm2 start server.js
